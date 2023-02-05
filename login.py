97f25751e382aafc93a68fb5013f16de22e1f846
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2023-02-05 18:59:06 -0300

    Cont.2

--- login.py
+++ login.py
@@ -10,14 +10,24 @@ janela.title('Sistema de Login')
 janela.iconbitmap("fogue2.ico")
 janela.resizable(False, False)
 
-#Trabalhando com a imagem da tela
+# Trabalhando com a imagem da tela
 img = PhotoImage(file='fog3.png')
 label_img = customtkinter.CTkLabel(master=janela, image=img)
 label_img.place(x=5, y=65)
 
-#frame
+# frame
 frame = customtkinter.CTkFrame(master=janela, width=350, height=396)
 frame.pack(side=RIGHT)
+
+
+# frame widgets
+label = customtkinter.CTkLabel(master=frame, text='Sistema de Login', text_font=('Roboto', 20))
+label.place(x=25, y=5)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Nome de Usuário", width=300, 
+text_font=('Roboto', 14)).place(x=25, y=105)
+label1= customtkinter.CTk(master=frame, text="*O campo nome de usuário é de caracter 
+obrigatório.", text_color='green', text_font=('Roboto', 8)).place(x=25, y=135)
 
 
 janela.mainloop()
