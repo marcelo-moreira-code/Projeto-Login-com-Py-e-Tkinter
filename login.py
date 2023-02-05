4a62750d68f60874b810f44fea4a903adf68bbf1
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2023-02-05 18:37:10 -0300

    mod img

--- login.py
+++ login.py
@@ -7,13 +7,17 @@ customtkinter.set_default_color_theme('dark-blue')
 janela = customtkinter.CTk()
 janela.geometry('700x400')
 janela.title('Sistema de Login')
-janela.iconbitmap("ico.ico")
+janela.iconbitmap("fogue2.ico")
 janela.resizable(False, False)
 
-
-img = PhotoImage(file='fog.png')
+#Trabalhando com a imagem da tela
+img = PhotoImage(file='fog3.png')
 label_img = customtkinter.CTkLabel(master=janela, image=img)
 label_img.place(x=5, y=65)
+
+#frame
+frame = customtkinter.CTkFrame(master=janela, width=350, height=396)
+frame.pack(side=RIGHT)
 
 
 janela.mainloop()
