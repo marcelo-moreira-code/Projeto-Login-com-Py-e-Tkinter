f1abd70b42b2164206d71d2851ed66eaea6e7932
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2023-02-05 17:23:05 -0300

    login
    body

---
+++ login.py
@@ -0,0 +1,13 @@
+import customtkinter
+from tkinter import *
+
+customtkinter.set_appearance_mode('dark')
+customtkinter.set_default_color_theme('dark-blue')
+
+janela = customtkinter.CTk()
+janela.geometry('700x400')
+janela.title('Sistema de Login')
+janela.iconbitmap("foguete.png")
+
+
+janela.mainloop()
