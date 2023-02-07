c82eba95ef5119b71f21154f962381cab816b019
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2023-02-06 21:38:10 -0300

    Update login.py
    Refratando e criando as classes e funcs

--- login.py
+++ login.py
@@ -1,46 +1,52 @@
 import customtkinter as ctk
 from tkinter import *
 
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("dark-blue")
-
 janela = ctk.CTk()
-janela.geometry("700x400")
-janela.title("Sistema de Login")
-janela.iconbitmap("fogue2.ico")
-janela.resizable(False, False)
 
-# Trabalhando com a imagem da tela
-img = PhotoImage(file="geo2.png")
-label_img = ctk.CTkLabel(master=janela, image=img)
-label_img.place(x=5, y=65)
+class Application():
+    def __init__(self):
+    
 
-label_title = ctk.CTkLabel(master=janela, text="Entre na sua conta e tenha\n a plataforma", 
-font=("Roboto", 18), text_color="#00B0F0").place(x=10, y=10)
-# Substitui text_font por font , pois , estava crashando !
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("dark-blue")
 
-# frame
-login_frame = ctk.CTkFrame(master=janela, width=350, height=396)
-login_frame.pack(side=RIGHT)
+    janela = ctk.CTk()
+    janela.geometry("700x400")
+    janela.title("Sistema de Login")
+    janela.iconbitmap("fogue2.ico")
+    janela.resizable(False, False)
 
-# frame widgets
-label = ctk.CTkLabel(master=login_frame, text="Sistema de Login", font=("Roboto", 20))
-label.place(x=25, y=5)
+    # Trabalhando com a imagem da tela
+    img = PhotoImage(file="geo2.png")
+    label_img = ctk.CTkLabel(master=janela, image=img)
+    label_img.place(x=5, y=65)
 
-username_entry = ctk.CTkEntry(master=login_frame, placeholder_text="Nome de Usuário", width=300, 
-font=("Roboto", 14)).place(x=25, y=105)
-username_label = ctk.CTkLabel(master=login_frame, text="* O campo nome de usuário é de caráter obrigatório.", 
-text_color="green", font=("Roboto", 8)).place(x=25, y=135)
+    label_title = ctk.CTkLabel(master=janela, text="Entre na sua conta e tenha\n a plataforma", 
+    font=("Roboto", 18), text_color="#00B0F0").place(x=10, y=10)
+    # Substitui text_font por font , pois , estava crashando !
 
-password_entry = ctk.CTkEntry(master=login_frame, placeholder_text="Senha de Usuário", width=300, 
-font=("Roboto", 14),show="*").place(x=25, y=175)
-password_label = ctk.CTkLabel(master=login_frame, text="*O campo Senha é de caráter obrigatório.", 
-text_color="green", font=("Roboto", 8)).place(x=25, y=205)
+    # frame
+    login_frame = ctk.CTkFrame(master=janela, width=350, height=396)
+    login_frame.pack(side=RIGHT)
 
-#Caixa manter-me conectado
-checkbox = ctk.CTkCheckBox(master=login_frame, text="Manter-me conectado sempre.").place(x=25, y=235)
+    # frame widgets
+    label = ctk.CTkLabel(master=login_frame, text="Sistema de Login", font=("Roboto", 20))
+    label.place(x=25, y=5)
 
-login_button = ctk.CTkButton(master=login_frame, text="Login", width=300).place(x=25, y=285)
+    username_entry = ctk.CTkEntry(master=login_frame, placeholder_text="Nome de Usuário", width=300, 
+    font=("Roboto", 14)).place(x=25, y=105)
+    username_label = ctk.CTkLabel(master=login_frame, text="* O campo nome de usuário é de caráter obrigatório.", 
+    text_color="green", font=("Roboto", 8)).place(x=25, y=135)
+
+    password_entry = ctk.CTkEntry(master=login_frame, placeholder_text="Senha de Usuário", width=300, 
+    font=("Roboto", 14),show="*").place(x=25, y=175)
+    password_label = ctk.CTkLabel(master=login_frame, text="*O campo Senha é de caráter obrigatório.", 
+    text_color="green", font=("Roboto", 8)).place(x=25, y=205)
+
+    #Caixa manter-me conectado
+    checkbox = ctk.CTkCheckBox(master=login_frame, text="Manter-me conectado sempre.").place(x=25, y=235)
+
+    login_button = ctk.CTkButton(master=login_frame, text="Login", width=300).place(x=25, y=285)
 
 
 janela.mainloop()
