d41bca96fd65090b84eed79ae1e1457ef0a09caa
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2023-02-07 22:35:26 -0300

    Update login.py

--- login.py
+++ login.py
@@ -67,6 +67,7 @@ class Application():
             
             label = ctk.CTkLabel(master=rg_frame, text="Faça o seu cadastro", font=("Roboto", 20)).place(x=25, y=5)
 
+            span = ctk.CTkLabel(master=rg_frame, text="*Todos os campos são de caráter obrigatório", font=("Roboto",10), text_color="gray").place(x=25, y=65)
 
             username_entry = ctk.CTkEntry(master=rg_frame, placeholder_text="Nome de Usuário", width=300, 
             font=("Roboto", 14)).place(x=25, y=105)
@@ -80,12 +81,15 @@ class Application():
             confirm_password_entry = ctk.CTkEntry(master=rg_frame, placeholder_text="Confirmar senha", width=300, 
             font=("Roboto", 14), show="*").place(x=25, y=225)
             
+            
             checkbox = ctk.CTkCheckBox(master=rg_frame, text="Aceito todos os Termos e Políticas.").place(x=25, y=265)
 
-            back_button = ctk.CTkButton(master=rg_frame, text="VOLTAR", width=145, fg_color="green", hover_color="#014B05").place(x=25, y=300)
-            save_button = ctk.CTkButton(master=rg_frame, text="CADASTRAR", width=145, fg_color="green", hover_color="014B05").place(x=180, y=300)
+            
+            back_button = ctk.CTkButton(master=rg_frame, text="VOLTAR", width=145, fg_color="green", hover_color="#202020").place(x=25, y=315)
+            save_button = ctk.CTkButton(master=rg_frame, text="CADASTRAR", width=145, fg_color="green", hover_color="014B05").place(x=180, y=315)
 
             pass
+        
         register_button = ctk.CTkButton(master=login_frame, text="Cadastre-se", width=150, 
         fg_color="green", hover_color="#2D9334", command=tela_register).place(x=175, y=325)
 
