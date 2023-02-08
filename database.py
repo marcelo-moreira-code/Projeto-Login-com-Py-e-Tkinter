720ef0007a0b79487e4f6ce8c71f3a6656c8c6b6
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2023-02-08 20:12:29 -0300

    Criando db

---
+++ database.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+conexao = sqlite3.connect("Sistema.db")
+
+cursor = conexao.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    confirmpassword TEXT NOT NULL,
+)""")
+
+print("Conexão ao Banco de Dados feita com Sucesso!")
